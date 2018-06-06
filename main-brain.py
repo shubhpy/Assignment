7766e4737aa242d6432c8f6ea4ec95ed2ee0c8fe
shubhpy <shubhpycode@gmail.com>
2018-06-06 18:07:33 +0530

    Added Cities and Working a little bit

--- main-brain.py
+++ main-brain.py
@@ -1,25 +1,86 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = "https://valiancesolutions.com"
+from csIndia import csIndia as lis
 
-page = requests.get(url).text
-soup = BeautifulSoup(page,"html.parser")
+# url = "https://valiancesolutions.com"
+urls = [
+    "https://valiancesolutions.com",
+    "http://corifictechnologies.com",
+    "https://www.hcltech.com",
+    "https://www.wipro.com",
+    "https://www.kpit.com",
+    "https://www.scatech.com",
+    "https://axtria.com",
+    "http://www.qcin.org"
+    ]
 
-for i in soup.findAll():
-    if i.text.lower() in ["contact us","contact","write","connect"]:
-        print (i)
+for url in urls:
+    page = requests.get(url).text
+    soup = BeautifulSoup(page,"html.parser")
+    
+    url2 = ""
+    for i in soup.findAll():
+        for j in ["contact us","contact","contact-us"]: 
+            if j in i.text.lower():
+                try:
+                    if i["href"]:
+                        url2 = i["href"]
+                except Exception as e:
+                    pass
+                break
 
-# got contact url somehow using href in i
+    if url2=="":
+        print ("couldn't find")
+        # use url as url2
+    else:
+        if url in url2:
+            print (url2)
+        else:
+            if url2[0]=="/":
+                print (url+url2)
+            else:
+                print (url+"/"+url2)                
 
-url2 = "https://valiancesolutions.com/contact-us/"
-page = requests.get(url2).text
-soup = BeautifulSoup(page,"html.parser")
+# def checkInChildren(city,temp):
+#     if temp and city and len(temp.findChildren()):
+#         for k in temp.findChildren():
+#             if city in k.text.lower():
+#                 address = k.text
+#                 address_raw,l = checkInChildren(city,k)
+#                 if address_raw==None:
+#                     if city in address.lower():
+#                         address_set.add(address)
+#                         break
+#         return (None,None)
+#     else:
+#         return (None,None)
 
-address = ""
-for i in soup.findAll("div",{"class":"vc_column-inner vc_custom_1489495538574"}):
-    if i.text!="":
-        address+=i.text
+# urls = [
+#     "https://valiancesolutions.com/contact-us/",
+#     "http://corifictechnologies.com/contact",
+#     "https://www.hcltech.com/contact-us/customer",
+#     "https://www.wipro.com/en-IN/contact-wipro/",
+#     "https://www.kpit.com/contact-us",
+#     "https://www.scatech.com/contact-us/overview/",
+#     "https://axtria.com/company/contact/",
+#     "http://www.qcin.org/contact.php"
+#     ]
 
-replaced = re.sub(r"\n+", "\n", address)
-print(replaced)
+# for url in urls: 
+#     page = requests.get(url).text
+#     soup = BeautifulSoup(page,"html.parser")
+
+#     i = soup.find("body")
+#     for j in lis:
+#         if j["city"] in i.text.lower():
+#             checkInChildren(j["city"],i)
+
+#     c = 0
+#     for i in address_set:
+#         if len(i)<200 and len(i)>20:
+#             print (c+1,i,"\n")
+#             c+=1
+    
+#     print ("\n\n\n")

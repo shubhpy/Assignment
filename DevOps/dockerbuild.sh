370a7412dad90d4d6ef5bae39090757e3daffdda
shubhpy <shubhpycode@gmail.com>
2018-06-08 17:21:48 +0530

    Edited DevOps Again

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DOCKERNAME="ValB"
 REPONAME="Assignment"
-CLONELINK="https://github.com/shubhpy/Assignment.git -b Backend"
-RUNSCRIPT=$REPONAME"/manage.py runserver 0.0.0.0:8000"
+CLONELINK="https://github.com/shubhpy/Assignment.git"
+RUNSCRIPT=$REPONAME
 
-docker exec -d $DOCKERNAME /bin/sh -c "pkill -SIGINT python;rm -rf "$REPONAME"/;git clone "$CLONELINK";python3.5 "$RUNSCRIPT
+docker exec -d $DOCKERNAME /bin/sh -c "pkill -SIGINT python;rm -rf "$REPONAME"/;git clone "$CLONELINK" -b Backend;python3.5 "$RUNSCRIPT"/manage.py runserver 0.0.0.0:8000"
 
 #Test Again
 #Test Again

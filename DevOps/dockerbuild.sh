95a036ba9326f92dfa30116e135c1d0d3293bcba
shubhpy <shubhpycode@gmail.com>
2018-06-08 16:43:32 +0530

    Testing GithubHook

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -5,3 +5,5 @@ CLONELINK="https://github.com/shubhpy/Assignment.git -b Backend"
 RUNSCRIPT=$REPONAME"/manage.py runserver 0.0.0.0:8000"
 
 docker exec -d $DOCKERNAME /bin/sh -c "pkill -SIGINT python;rm -rf "$REPONAME"/;git clone "$CLONELINK";python3.5 "$RUNSCRIPT
+
+#Test Again
